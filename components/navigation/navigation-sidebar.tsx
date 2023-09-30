3d9fbb87c560d688e907905e866db21417b2482f
daburndev <daburndev@gmail.com>
2023-09-30 22:32:22 +0700

    Update navigation-sidebar.tsx

--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -49,7 +49,14 @@ export const NavigationSidebar = async () => {
             </ScrollArea>
             <div className="pb-3 mt-auto flex items-center flex-col gap-y-4">
                 <ModeToggle />
-                <UserButton />
+                <UserButton
+                    afterSignOutUrl="/"
+                    appearance={{
+                        elements: {
+                        avatarBox:"h-[48px] w-[48px]"
+                        }
+                    }}
+                />
             </div>
         </div>
     )
